b65c34ab082bf28fb0a4d3badb2641285913f94b
Laura Beall <lbeall3@gatech.edu>
2016-09-20 18:40:46 -0400

    Linked some buttons and added pop up menus

--- app/src/main/java/io/github/tipline/android_app/MainPage.java
+++ app/src/main/java/io/github/tipline/android_app/MainPage.java
@@ -1,14 +1,32 @@
 package io.github.tipline.android_app;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainPage extends AppCompatActivity {
+
+    private ListView DrawerList;
+    private ArrayAdapter<String> adapter;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle DrawerToggle;
+    private String ActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +61,94 @@ public class MainPage extends AppCompatActivity {
 
             }
         });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActivityTitle = getTitle().toString();
+
+        DrawerList = (ListView)findViewById(R.id.navList);
+        addDrawerItems();
+
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        DrawerList.setAdapter(adapter);
+
+        DrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainPage.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private void setupDrawer() {
+        DrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(ActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        DrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(DrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        DrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        DrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (DrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
--- app/src/main/java/io/github/tipline/android_app/TipMenuFragment.java
+++ app/src/main/java/io/github/tipline/android_app/TipMenuFragment.java
@@ -1,18 +1,51 @@
 package io.github.tipline.android_app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
+public class TipMenuFragment extends Fragment implements View.OnClickListener {
 
-public class TipMenuFragment extends Fragment {
+    private Button textTipButton;
+    private Button tipCallButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tip_menu, container, false);
+        View myView = inflater.inflate(R.layout.fragment_tip_menu, container, false);
+        textTipButton = (Button) myView.findViewById(R.id.button_tip_text);
+        textTipButton.setOnClickListener(this);
+        tipCallButton = (Button) myView.findViewById(R.id.button_tip_call);
+        tipCallButton.setOnClickListener(this);
+        return myView;
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.button_tip_text:
+                Intent textTipPage = new Intent(getActivity().getApplication(), TextTip.class);
+                this.startActivity(textTipPage);
+                break;
+
+            case R.id.button_tip_call:
+                Intent myIntent = new Intent(getActivity().getApplication(), TipCall.class);
+                this.startActivity(myIntent);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
 }
