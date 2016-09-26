4cfaaf7eaa6142d9ff854a2b51398d8993cc4047
Jackie Joyce <pbjcjoyce@yahoo.com>
2016-09-25 22:11:01 -0400

    fixed audio tip layout and added tip call placeholder

--- app/src/main/java/io/github/tipline/android_app/TipCall.java
+++ app/src/main/java/io/github/tipline/android_app/TipCall.java
@@ -1,13 +1,19 @@
 package io.github.tipline.android_app;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class TipCall extends AppCompatActivity {
+public class TipCall extends AppCompatActivity implements View.OnClickListener {
+
+    private Button submitButton;
+    private Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,80 @@ public class TipCall extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        submitButton = (Button) findViewById(R.id.submit_call);
+        cancelButton = (Button) findViewById(R.id.cancel_call);
+
+        submitButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.submit_call:
+                showConfirmationDialog();
+                break;
+            case R.id.cancel_call:
+                showCancellationDialog();
+
+            default:
+                break;
+        }
+
+    }
+
+
+    private void showConfirmationDialog() {
+
+        AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
+        helpBuilder.setTitle("Confirm Call Tip?");
+        helpBuilder.setMessage("Use this message? The message will be " +
+                "sent to law enforcement officials to investigate this suspicion of human trafficking.");
+        helpBuilder.setPositiveButton("Confirm",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing but close the dialog
+                    }
+                });
+        helpBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Nothing but close dialog box
+                    }
+                });
+
+        // Remember, create doesn't show the dialog
+        AlertDialog helpDialog = helpBuilder.create();
+        helpDialog.show();
+    }
+
+    private void showCancellationDialog() {
+
+        AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
+        helpBuilder.setTitle("Cancel Call Tip?");
+        helpBuilder.setMessage("Are you sure you want to cancel this message? " +
+                "Your message and any attachments will be lost and will not be sent to the authorities");
+        helpBuilder.setPositiveButton("Return to Message",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing but close the dialog
+                    }
+                });
+        helpBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Nothing but close dialog box
+                    }
+                });
+
+        // Remember, create doesn't show the dialog
+        AlertDialog helpDialog = helpBuilder.create();
+        helpDialog.show();
+    }
 }
