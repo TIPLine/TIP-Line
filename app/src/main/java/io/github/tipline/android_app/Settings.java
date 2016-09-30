86754c6a9139c4aa9b5d6e8c6fe1214c1ffc41fa
Piya Saetiew <Piya Saetiew>
2016-09-30 07:22:13 -0400

    Added back button to settings page.

--- app/src/main/java/io/github/tipline/android_app/Settings.java
+++ app/src/main/java/io/github/tipline/android_app/Settings.java
@@ -2,6 +2,7 @@ package io.github.tipline.android_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
