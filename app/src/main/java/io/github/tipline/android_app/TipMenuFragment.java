aa2d8c108958326a98b2e9a0277d31e835027048
arianawaugh <waugh.ariana@gmail.com>
2016-10-19 11:36:02 -0400

    Phone call push to TipMenuFragment

--- app/src/main/java/io/github/tipline/android_app/TipMenuFragment.java
+++ app/src/main/java/io/github/tipline/android_app/TipMenuFragment.java
@@ -1,15 +1,22 @@
 package io.github.tipline.android_app;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
 
 public class TipMenuFragment extends Fragment implements View.OnClickListener {
 
@@ -67,15 +74,85 @@ public class TipMenuFragment extends Fragment implements View.OnClickListener {
 
     }
 
-    private void call() {
-        Intent in=new Intent(Intent.ACTION_CALL, Uri.parse("6789100416"));
-        try{
-            startActivity(in);
-        }
+    public void call() {
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CALL_PHONE);
 
-        catch (android.content.ActivityNotFoundException ex){
-            Toast.makeText(getActivity(),"yourActivity is not founded",Toast.LENGTH_SHORT).show();
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    getActivity(), new String[]{Manifest.permission.CALL_PHONE}, 123);
+        } else {
+
+            Intent in = new Intent(Intent.ACTION_CALL, Uri.parse("tel:6789100416"));
+            try {
+                startActivity(in);
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(getActivity(), "yourActivity is not founded", Toast.LENGTH_SHORT).show();
+            }
         }
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+
+            case 123:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    call();
+                } else {
+                    Log.d("TAG", "Call Permission Not Granted");
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    //monitor phone call activities
+    private class PhoneCallListener extends PhoneStateListener {
+
+        private boolean isPhoneCalling = false;
+
+        String LOG_TAG = "LOGGING 123";
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            if (TelephonyManager.CALL_STATE_RINGING == state) {
+                // phone ringing
+                Log.i(LOG_TAG, "RINGING, number: " + incomingNumber);
+            }
+
+            if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+                // active
+                Log.i(LOG_TAG, "OFFHOOK");
+
+                isPhoneCalling = true;
+            }
+
+            if (TelephonyManager.CALL_STATE_IDLE == state) {
+                // run when class initial and phone call ended,
+                // need detect flag from CALL_STATE_OFFHOOK
+                Log.i(LOG_TAG, "IDLE");
+
+                if (isPhoneCalling) {
+
+                    Log.i(LOG_TAG, "restart app");
+
+                    // restart app
+                    Intent i = getActivity().getBaseContext().getPackageManager()
+                            .getLaunchIntentForPackage(
+                                    getActivity().getBaseContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+
+                    isPhoneCalling = false;
+                }
+
+            }
+        }
+    }
+
 
 }
